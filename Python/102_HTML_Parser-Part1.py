b21f786d1c0f23682ff39d5a8b6fd752f059023a
Marc Atef <64839030+Marcoc51@users.noreply.github.com>
2022-10-08 09:25:11 +0200

    Create 102_HTML_Parser-Part1.py

---
+++ Python/102_HTML_Parser-Part1.py
@@ -0,0 +1,19 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start :", tag)
+        for attr in attrs:
+            print("->", attr[0], ">", attr[1])
+
+    def handle_endtag(self, tag):
+        print("End   :", tag)
+
+    def handle_startendtag(self, tag, attrs):
+        print("Empty :", tag)
+        for attr in attrs:
+            print("->", attr[0], ">", attr[1])
+
+parser = MyHTMLParser()
+for _ in range(int(input())):
+    parser.feed(input())
